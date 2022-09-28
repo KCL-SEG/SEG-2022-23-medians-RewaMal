6533f6217ffceff876fcbd344444bcd9751b0caf
RewaMal <114403485+RewaMal@users.noreply.github.com>
2022-09-28 18:22:19 +0100

    Update median.py

--- median.py
+++ median.py
@@ -9,17 +9,17 @@ while True:
         sorted_number = sorted(numbers)
         #find the number of elements
         length = len(numbers)
-        #print(sorted_number)
+        
         #check if number of elements is even or odd
         if length %2 == 0: #it is even number of elements
             median = (sorted_number[length //2 - 1] + sorted_number[length // 2 ]) /2
-            #print(f'{median}')   
+            print(f'{median}')   
         else: #it is odd number of elements
             median = sorted_number[length //2] 
-            #print(f'{median}')      
+            print(f'{median}')      
         
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
