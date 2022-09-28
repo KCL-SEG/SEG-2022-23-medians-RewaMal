64f51420fd20141ff7133f377d4fbc75e34dbdec
RewaMal <114403485+RewaMal@users.noreply.github.com>
2022-09-28 09:55:24 +0100

    Add solution to medians.py

--- median.py
+++ median.py
@@ -2,9 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
-    try:
+     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        #sort the list
+        sorted_number = sorted(numbers)
+        #find the number of elements
+        length = len(numbers)
+        
+        #check if number of elements is even or odd
+        if length %2 == 0: #it is even number of elements
+            median = (sorted_number[length //2 - 1] + sorted_number[length // 2 ]) /2
+            print(f'the median of {numbers} is {median}')   
+        else: #it is odd number of elements
+            median = sorted_number[length //2] 
+            print(f'the median of {numbers} is {median}')      
+        print(sorted_number)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
