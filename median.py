5e7cd1a3984d48304c7d39fbb1dd5796c5c36a47
RewaMal <114403485+RewaMal@users.noreply.github.com>
2022-09-28 10:06:59 +0100

    Update solution for medians.py
    Change the output of the code to match the autograding excepted result

--- median.py
+++ median.py
@@ -13,11 +13,11 @@ while True:
         #check if number of elements is even or odd
         if length %2 == 0: #it is even number of elements
             median = (sorted_number[length //2 - 1] + sorted_number[length // 2 ]) /2
-            print(f'the median of {numbers} is {median}')   
+            print(f'{median}')   
         else: #it is odd number of elements
             median = sorted_number[length //2] 
-            print(f'the median of {numbers} is {median}')      
-        print(sorted_number)
+            print(f'{median}')      
+        
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
